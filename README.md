254d9609c49ba63114a67db74ee2a329109f3b4e
Tony McCoy <toonytony@gmail.com>
2015-08-22 00:17:07 -0700

    clean up

---
+++ README.md
@@ -0,0 +1,62 @@
+# Typewriter.js
+
+## Getting Started
+
+```
+typewriter.write(text[, speed, callback, wait time ]);
+```
+
+### Parameters
+
+_**text**_<br>
+A string containing the text you want to print.
+	
+_**speed**_<br>
+_Optional._ A number representing the speed you want Typewriter to type the text. Default is 1. Setting speed to 0 will print your text immediately, skipping the typing effect. Higher numbers print faster while lower numbers print slower.
+
+_**wait time**_<br>
+_Optional._ Milliseconds to wait before executing the callback. Useful if you want to pause for dramatic effect or wait for a time before clearing the screen, for example.
+
+_**callback**_<br>
+_Optional._ A function to execute after the text is done being written. This can be another typewriter.write() or typewriter.clear() for example.
+
+
+### All Together Now
+
+```
+typewriter.write("Hello World!", 0.5, function() {
+	console.log("Printed Hello World!");
+}, 500);
+```
+
+The code above will print "Hello World!" at half the default speed, then wait half a second before logging "Printed Hello World!" to the console. Easy, right? Of course the console.log and delay are unnecessary, so it would probably look more like this on a real website;
+
+```
+typewriter.write("Hello World!", 0.5);
+```
+
+
+## Chaining
+
+After you've played around with Typewriter a bit, you will probably find yourself wanting to execute a series of typewriter.write() commands in a specific order, for more granular control over the timing and speed of each section of text. 
+
+Let's say you wanted to write "One Two Three Four", clearing the screen and cutting the typing speed in half for each word. You could do
+
+```
+typewriter.write("One", 1, function() {
+	typewriter.clear(function() {
+		typewriter.write("Two", 0.5, function() {
+			typewriter.clear(function() {
+				typewriter.write("Three", 0.25, function() {
+					typewriter.clear(function() {
+						typewriter.write("Four", 0.125);
+					});
+				}, 250);
+			})
+		}, 250);
+	});
+}, 250);
+```
+
+It's not an optimal solution, but I'm pretty new at JavaScript and I'm still getting the hang of Asynchronous functions. If you know of a simple enough way to get that under control, submit a pull request!
+
--- js/typewriter.js
+++ js/typewriter.js
@@ -1,139 +1,100 @@
-var t = Typewriter = {
-	settings: {
+/**************************
+ * typewriter.js v1.0.0
+ * 
+ * Copyright (c) 2015 Tony McCoy
+ * http://www.tonymccoy.me
+ * 
+ * This software is distributed under the MIT License
+ * http://opensource.org/licenses/MIT
+ */
+
+var typewriter = (function () {
+	
+	var options = {
 		speed: 1,
-		input: "#input",
-		output: "#output",
-		typeCursor: "#type-cursor",
-		userInput: "#user-prompt",
-		userCursor: "#user-cursor",
-		punctuationDelay: true,
-		soundEffects: false
-	}
-};
-
-function turnInput(action, callback) {
-	var userInput = document.querySelector(t.settings.userInput),
-		userCursor = document.querySelector(t.settings.userCursor),
-		typeCursor = document.querySelector(t.settings.typeCursor);
+		outputElement: "#output",
+		cursor: "#type-cursor",
+		punctuationDelay: true
+	};
 	
-	if (action === "on") {
-		// Unhide and enable user input
-		input.innerHTML = "";
-		userInput.classList.remove("disabled");
-		typeCursor.classList.add("disabled");
-		setTimeout(function(){
-			userCursor.classList.remove("disabled");
-		}, 500);
-		document.addEventListener("keydown", function(event) { input(event); });
-	} else if (action === "off") {
-		// Hide and disable user input
-		document.removeEventListener("keydown");
-		userInput.classList.add("disabled");
-		userCursor.classList.add("disabled");
-		typeCursor.classList.remove("disabled");
+	// Time to pause for punctuation (in milliseconds)
+	var punctuationTiming = {
+		'.': 400,
+		',': 250,
+		'?': 400,
+		'!': 400
 	}
 	
-	if (callback) { callback(); }
-}
-
-function write(text, speed, callback, wait) {
-	turnInput('off');
-	
-	var output = document.querySelector(t.settings.output),
-		charsRemaining = text.length,
-		text = decodeURI(text);
-
-	var printChar = function () {
-		if (charsRemaining > 0) {
-			var char = text[text.length - charsRemaining],
-				nextChar = text[text.length - charsRemaining + 1],
-				lastChar = text[text.length - charsRemaining - 1];
-			output.insertAdjacentHTML("beforeend", char);
-			charsRemaining -= 1;
-			
-			var delay = 60;
-			
-			// Someone, please slap me for what I'm about to do...
-			// If you know a better way to define delays for particular characters,
-			// submit a pull request and we'll pretend this never happened. :x
-			
-			if (t.settings.punctuationDelay) {
-				if (char === "?" || char === "!") {
-					if (nextChar === " ") { delay = 750; } // End of sentence punctuation
-				} else if (char === ".") {
-					if (lastChar !== "." && nextChar === " ") { delay = 750; } // end of sentence
-					else if (nextChar === "." || lastChar === ".") { delay = 400; } // ellipsis
-					else if (nextChar === " " && lastChar === ".") { delay = 400; } // end of ellipsis
-					else { delay = 750; } // period catchall
-				} else if (char === "-") { delay = 250; } // dash 
-				else { delay = 60; } // catchall or normal letter
+	var runCallback = function (callback, wait) {
+		if (callback) {
+			if (wait) {
+				setTimeout(function () { callback(); }, wait);
+			} else {
+				callback();
 			}
+		}
+	};
+	
+	var global = {
+		config: function (obj) {
+			var keys = Object.keys(obj);
+			for (var i = 0; i < obj.length; i++) options[keys[i]] = obj[i];
+		},
+		write: function (text, speed, wait, callback) {
 			
-			setTimeout(function () {
-				printChar(); // repeat this function if more characters need printing
-			}, delay * speed);
-			
-		} else {
-			if (callback) {
-				if (wait) {
-					setTimeout(function() { callback(); }, wait);
-				} else {
-					callback();
+			// If no speed is specified, fall back to default speed
+			if (speed || speed === 0) {
+				if (typeof speed === "function") {
+					if (callback) wait = callback;
+					callback = speed;
+					speed = options.speed;
 				}
+			} else { speed = options.speed; }
+			
+			var out = document.querySelector(options.outputElement),
+				cursor = document.querySelector(options.cursor),
+				charsRemaining = text.length;
+		
+			if (speed === 0) { // Print all text instantly
+				out.insertAdjacentHTML("beforeend", text);
+			} else {
+				cursor.style.display = "inline-block"; // enable cursor
+				
+				(function charLoop(i) {
+					var char = text[text.length - i],
+						nextChar = text[text.length - i + 1],
+						lastChar = text[text.length - i - 1],
+						delay = 30;
+						
+					if (options.punctuationDelay) {
+						if (char === ".") {
+							delay = punctuationTiming[char];
+							if (nextChar === ".") delay = punctuationTiming[char] / 2;
+							if (lastChar === ".") delay = punctuationTiming[char] / 2;
+						} else if (/[,?!]/.test(char) && nextChar === " ") {
+							delay = punctuationTiming[char];
+						}
+					}
+					
+					out.insertAdjacentHTML("beforeend", char);
+					
+					setTimeout(function () {
+						if (--i) {
+							charLoop(i);
+						} else {
+							if (!callback) setTimeout(function () {
+								cursor.style.display = "none";
+							}, 300); // disable cursor
+							runCallback(callback, wait);
+						}
+					}, delay / speed);
+				})(text.length);
 			}
+		},
+		clear: function (callback, wait) {
+			document.querySelector(options.outputElement).innerHTML = "";
+			runCallback(callback, wait);
 		}
-	}
-	
-	printChar();
-}
-
-function clear(callback, wait) {
-	document.querySelector(t.settings.output).innerHTML = "";
-	
-	if (callback) {
-		if (wait) {
-			setTimeout(function() { callback(); }, wait);
-		} else {
-			callback();
-		}
-	}
-}
-
-function parse(val) {
-	turnInput('off');
-	if (val === "SETTINGS") {
-		console.log("Running SETTINGS");
-	} else if (val === "RESTART") {
-		console.log("Running RESTART");
-	} else {
-		var responses = [
-			"Sorry. I'm not quite sure what you mean by that.",
-			"Hmm. You feeling okay, there?",
-			"Dear God. WHAT IS HE TRYING TO SAY?",
-			"Try SETTINGS. I think you'll like SETTINGS.",
-			"Oh, sure. Go ahead and waste your time. I swear there are no easter eggs here. Really!"
-		]
-		clear(function() { 
-			write(_.sample(responses), 0.6, function () {
-				turnInput('on');
-			});
-		});
-	}
-}
-
-function input(e) {
-	var input = document.querySelector(t.settings.input);
-	var char = String.fromCharCode(e.keyCode);
-	
-	if (e.which === 8) { // backspace or delete
-		str = input.innerHTML;
-		str = str.substring(0, str.length - 1);
-		input.innerHTML = str;
-	} else if (e.which === 13){ // enter
-		e.preventDefault();
-		parse(input.innerHTML);
-		input.innerHTML = "";
-	} else {
-		input.insertAdjacentHTML("beforeend", char.toUpperCase());
-	}
-}
+	};
+	return global;
+}());
